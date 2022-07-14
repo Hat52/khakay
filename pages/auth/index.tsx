3cc715f1a160361df96b67434effc10aa6f5e551
Hat52 <tanolihamzaali@gmail.com>
2022-07-14 20:50:18 +0500

    enhancement: Integration

---
+++ pages/auth/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+const Auth: NextPage = () => {
+    return(
+        <h1>Welcome to Auth template gallery</h1>
+    )
+}
+
+export default Auth
---
+++ pages/cards/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+const Cards: NextPage = () => {
+    return(
+        <h1>Welcome to Cards template gallery</h1>
+    )
+}
+
+export default Cards
