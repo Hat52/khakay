d5f7742c0f5ac2b8d900ed921bdb56a91e70367a
Hat52 <tanolihamzaali@gmail.com>
2022-07-25 09:55:09 +0500

    -

--- pages/cards/components/ViewCard/index.tsx
+++ pages/cards/components/ViewCard/index.tsx
@@ -7,7 +7,7 @@ const ViewCard: NextPage<any> = ({component:Component,isPreview,path}) => {
     return(
         <Link href={path}>
             <div className={Styles.container}>
-                <Component isPreview={isPreview}/>
+                <Component isPreview={isPreview}/> 
             </div>
         </Link>
     )
--- pages/page-not-found/index.tsx
+++ pages/page-not-found/index.tsx
@@ -1,34 +1,9 @@
-// import type { NextPage } from 'next'
-// import Link from 'next/link'
-// import Image from 'next/image'
-// // import PageUnderConstruction from '../coming-soon'
-// import Styles from './styles/not-found.module.css'
-// const PageNotFound: NextPage = () => {
-//     return(
-//         // <PageUnderConstruction/>
-//         <div className={Styles.page_one_container}>
-//             <Link href="/">Back To Home</Link>
-//             <h3>
-//                 404<br/>
-//                 Lost directions
-//             </h3>
-//             <div className={Styles.image_container}>
-//                 <Image src="/img/lost-direction.svg" alt="me"  layout='fill' objectFit='contain' /> 
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default PageNotFound
-
-import type { NextPage } from 'next'
-import Link from 'next/link'
-import Image from 'next/image'
-// import PageUnderConstruction from '../coming-soon'
-import Styles from './styles/not-found.module.css'
-import StylesMain from '../../styles/Home.module.css'
-import ViewCard from '../cards/components/ViewCard'
-import {PageNotFound1} from '../../components/page-not-found'
+import type { NextPage } from 'next';
+// import PageUnderConstruction from '../coming-soon';
+import Styles from './styles/not-found.module.css';
+import StylesMain from '../../styles/Home.module.css';
+import ViewCard from '../cards/components/ViewCard';
+import {PageNotFound1} from '../../components/page-not-found';
 const PageNotFound: NextPage = () => {
     return(
         <div className="page-conatiner">
@@ -43,7 +18,6 @@ const PageNotFound: NextPage = () => {
                                 <ViewCard component={PageNotFound1} isPreview={true} path={`/page-not-found/pages/0`}/>
                             </div>
                         )
-
                     })
                 }
             </div>
