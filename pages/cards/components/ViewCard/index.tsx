3dbcf0c7756a9d1d052fbc68b4f68e530fdd96a0
Hat52 <tanolihamzaali@gmail.com>
2022-07-25 10:03:07 +0500

    resolved: href issue

--- pages/cards/components/ViewCard/index.tsx
+++ pages/cards/components/ViewCard/index.tsx
@@ -5,11 +5,15 @@ import PageUnderConstruction from '../../../coming-soon'
 import Link from 'next/link'
 const ViewCard: NextPage<any> = ({component:Component,isPreview,path}) => {
     return(
-        <Link href={path}>
-            <div className={Styles.container}>
-                <Component isPreview={isPreview}/> 
-            </div>
-        </Link>
+        <>
+            {path?
+                <Link href={path}>
+                    <div className={Styles.container}>
+                        <Component isPreview={isPreview}/> 
+                    </div>
+                </Link>:
+            null}
+        </>
     )
 }
 
