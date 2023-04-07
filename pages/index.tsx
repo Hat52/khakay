c248c1b5d1ed9ecd747d8d8102efa42a5019a98a
hamza ali <tanolihamzaali@gmail.com>
2023-04-08 01:59:04 +0500

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,79 +1,64 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Khakay</title>
-        <meta name="description" content="Khakay" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Khakay</title>
+				<meta name="description" content="Khakay" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Khakay!
-        </h1>
+			<main className={styles.main}>
+				<h1 className={styles.title}>Welcome to Khakay!</h1>
 
-        <div className={styles.grid}>
-          <Link href="/auth">
-            <a className={styles.card}>
-              <h2>Auth Forms &rarr;</h2>
-              <p>Signin and Logup forms Templates</p>
-            </a>
-          </Link>
-          <Link href="/cards">
-            <a className={styles.card}>
-              <h2>Cards &rarr;</h2>
-              <p>Different type of cards Templates</p>
-            </a>
-          </Link>
-          <Link href="/multi-step-form">
-            <a className={styles.card}
-            >
-              <h2>Multi Step Forms &rarr;</h2>
-              <p>Beautiful Multi step forms Templates</p>
-            </a>
-          </Link>
-          <Link href="/navbar">
-            <a
-              className={styles.card}
-            >
-              <h2>Navbars &rarr;</h2>
-              <p>
-                Beautiful Navbars Templates
-              </p>
-            </a>
-          </Link>
-          <Link href="/page-not-found">
-            <a
-              className={styles.card}
-            >
-              <h2>Page Not Found &rarr;</h2>
-              <p>
-                Beautiful Page Not Found Templates
-              </p>
-            </a>
-          </Link>
-        </div>
-      </main>
+				<div className={styles.grid}>
+					<Link href="/auth">
+						<a className={styles.card}>
+							<h2>Auth Forms &rarr;</h2>
+							<p>Signin and Logup forms Templates</p>
+						</a>
+					</Link>
+					<Link href="/cards">
+						<a className={styles.card}>
+							<h2>Cards &rarr;</h2>
+							<p>Different type of cards Templates</p>
+						</a>
+					</Link>
+					<Link href="/multi-step-form">
+						<a className={styles.card}>
+							<h2>Multi Step Forms &rarr;</h2>
+							<p>Beautiful Multi step forms Templates</p>
+						</a>
+					</Link>
+					<Link href="/navbar">
+						<a className={styles.card}>
+							<h2>Navbar &rarr;</h2>
+							<p>Beautiful Navbar Templates</p>
+						</a>
+					</Link>
+					<Link href="/page-not-found">
+						<a className={styles.card}>
+							<h2>Page Not Found &rarr;</h2>
+							<p>Beautiful Page Not Found Templates</p>
+						</a>
+					</Link>
+				</div>
+			</main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://github.com/Hat52"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Devloped by Hamza Ali
-          {/* <span className={styles.logo}>
+			<footer className={styles.footer}>
+				<a href="https://github.com/Hat52" target="_blank" rel="noopener noreferrer">
+					Devloped by Hamza Ali
+					{/* <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span> */}
-        </a>
-      </footer>
-    </div>
-  )
-}
+				</a>
+			</footer>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
--- pages/page-not-found/index.tsx
+++ pages/page-not-found/index.tsx
@@ -3,30 +3,29 @@ import type { NextPage } from 'next';
 import Styles from './styles/not-found.module.css';
 import StylesMain from '../../styles/Home.module.css';
 import ViewCard from '../cards/components/ViewCard';
-import {PageNotFound1,PageNotFound2} from '../../components/page-not-found';
+import { PageNotFound1, PageNotFound2 } from '../../components/page-not-found';
 const PageNotFound: NextPage = () => {
-    return(
-        <div className="page-conatiner">
-            <h1 className="page-heading" >
-                404 Page Templates
-            </h1>
-            <div className={Styles.not_found_card_container}>
-                {/* {
-                    [0,1,2,3,4].map((value,index)=>{
-                        return( */}
-                            <div >
-                                <ViewCard component={PageNotFound1} isPreview={true} path={`/page-not-found/pages/0`}/>
-                            </div>
-                            <div >
-                                <ViewCard component={PageNotFound2} isPreview={true} path={`/page-not-found/pages/404-page-2`}/>
-                            </div>
-                            
-                        {/* )
-                    }) */}
-                {/* } */}
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="page-conatiner">
+			<h1 className="page-heading">404 Page Templates</h1>
+			<div className={Styles.not_found_card_container}>
+				<div>
+					<ViewCard
+						component={PageNotFound1}
+						isPreview={true}
+						path={`/page-not-found/pages/0`}
+					/>
+				</div>
+				<div>
+					<ViewCard
+						component={PageNotFound2}
+						isPreview={true}
+						path={`/page-not-found/pages/404-page-2`}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default PageNotFound
+export default PageNotFound;
